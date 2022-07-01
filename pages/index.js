7bd8ad04c5b2dfadcf9d7d358dd6f42d66353393
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-01 11:47:52 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className={styles.home}>
       <Htmlheader title='Skills Test' />
-      <h1>Hello My Name Is Nicholas Houston</h1>
-      <p>For your consideration, please follow me through the following questions and scenerios.</p>
+      <h1>Dawn Jama</h1>
+      <p>Skill test for product advocate role</p>
       <Link href='/focus'>Lets Get Started</Link>
     </div>
   )
