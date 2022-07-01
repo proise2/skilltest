80da442e93a08c2df601965150bb13bc23f59c9e
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-01 12:06:49 -0600

    Update past.js

--- pages/past.js
+++ pages/past.js
@@ -5,7 +5,7 @@ import styles from '../styles/Question.module.css'
 const past = () => {
   return (
     <div className={styles.container}>
-      <Htmlheader title='Work Goals' />
+      <Htmlheader title='Past' />
       <h2>What do you want to learn, or do more of, at work?</h2>
       <p>Currently I do not have much time outside of tickets and working with our team. I really want to help our product team develop and create better tools for our support team to utilize.</p>
       <p>There are a few logging utilities that I think could be made better, or else have a nicer UI to display the logs. A lot of the time our logs are out of chronological order, so just creating a tool to put them in an easier to read and in order by their time stamp would help increase troubleshooting speed.</p>
