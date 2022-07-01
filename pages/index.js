06e71e7e77987848497534811baa16f1f67f7e43
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-01 12:41:22 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,22 @@ export default function Home() {
       <Htmlheader title='Skills Test' />
       <h1>Dawn Jama</h1>
       <p>Skill test for product advocate role</p>
+
+<img src=".jpg" loading="eager" alt="Dawn Jama Profile Photo" class="image">
+
+Hi, I'm a former teacher and aspiring product advocate. 
+
+This portfolio is built with **Next.js** and will showcase my skill test for a product advocate role.
+
+ <Link href='/product'>Lets Get Started</Link>
+
+---
+
+- Twitter [@byeAJ1](https://twitter.com/ByeAJ1)
+- GitHub [@proise2](https://github.com/proise2)
+- Email dawnjama2@gmail.com
+                    
+                   
       <Link href='/product'>Lets Get Started</Link>
     </div>
   )
