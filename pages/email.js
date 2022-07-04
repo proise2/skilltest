8b495425840b71ae9c52002940c6846e535b2f26
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 10:58:47 -0600

    Update email.js

--- pages/email.js
+++ pages/email.js
@@ -12,13 +12,13 @@ const email = () => {
           <p>Please include one email response for each inquiry, just as if you were responding to a customer.</p>
         </li>
         <li>
-<P><b>Please include one email response for each inquiry, just as if you were responding to a customer.</b>
-<P><b>Sales Inquiry:</b> Do I need Enterprise? 
-<P><b>Subject:</b> Sales Lead for barnesandnoble.com
-<P><b>Name:</b> Joe Fox
-<P><b>Employees:</b> 1000+
+<P><b>Please include one email response for each inquiry, just as if you were responding to a customer.</b></p>
+<P><b>Sales Inquiry:</b> Do I need Enterprise? </p>
+<P><b>Subject:</b> Sales Lead for barnesandnoble.com</p>
+<P><b>Name:</b> Joe Fox</p>
+<P><b>Employees:</b> 1000+</p>
 <P>How can we help you: New site launching soon using Next.js and we are thinking about using Vercel
-to host. I would like more information about the Enterprise option.
+to host. I would like more information about the Enterprise option.</p>
  </li>
 <li><P>There are a few things to consider when choosing a hosting provider for your next project. Vercel has a lot of great features, but it's important to make sure that the provider has the resources and capabilities to support your project. </P>
 
