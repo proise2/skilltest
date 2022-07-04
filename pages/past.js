6261586fc4d9bc409b50ee0c757bd216b6a7d45e
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 14:22:06 -0600

    Update past.js

--- pages/past.js
+++ pages/past.js
@@ -4,6 +4,7 @@ import styles from '../styles/Question.module.css'
 
 const past = () => {
   return (
+    
     <div className={styles.container}>
       <Htmlheader title='Past' />
       <h2> Reflection on past experience:</h2>
@@ -11,7 +12,7 @@ const past = () => {
         <p> I'm looking to learn more about the products that I'm selling and how they can be used in different industries. I also want to continue developing my sales skills and learn more about how to close deals.</p>
       <p> <b> b. Which (non-Vercel) technical or sales article, guide or lightning talk left the biggest positive impression on you? Why? (and please provide the link) </b></p>
 <p>I really enjoyed a technical talk by Rich Hickey called "Simple Made Easy." He talked about how we tend to overcomplicate things and how we can make our lives easier by keeping things simple. I found his talk motivating and inspiring. </p>
-      <p> <a href="https://www.automasean.blog/simple-made-easy/">Simple Made Easy</a> </p>
+      <div className={styles.link}><Link href='https://www.automasean.blog/simple-made-easy/'>Simple Made Easy blog</Link></div>
       <div className={styles.link}><Link href='/redirect'>Lets try using Next.js</Link></div>
     </div>
   )
