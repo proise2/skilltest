b7d0391a722b10de758c9e7aaed4e5b84421f68e
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 14:18:55 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export default function Home() {
     <div className={styles.home}>
       <Htmlheader title='Skills Test' />
       <h1>Hi, My name Is Dawn Jama</h1>
-<img src="https://pbs.twimg.com/profile_images/1496880979482349570/kvrdCaGn_400x400.jpg" alt="Dawn Jama" width="500" height="600">
       <p>I'm a former teacher and aspiring product advocate.</p>
       <p>Here you will find my attempt at using Vercel to create a project.</p>
       <p> Using the links below to see my answers for the product adovcate skilltest for Vercel.</p>
