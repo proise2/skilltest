4715cf5efd60819b59d18adc62b549797a25b2a0
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 13:29:22 -0600

    Update email.js

--- pages/email.js
+++ pages/email.js
@@ -9,17 +9,39 @@ const email = () => {
       <h2>Practice Email Responses</h2>
       <ul className={styles.card_vert}>
                <li>
-<p><b>Please include one email response for each inquiry, just as if you were responding to a customer.</b></p>
-<p><b>Sales Inquiry:</b> Do I need Enterprise? </p>
+<p><b><u>Sales Inquiry: Do I need Enterprise? </u></b></p>
 <p><b>Subject:</b> Sales Lead for barnesandnoble.com</p>
 <p><b>Name:</b> Joe Fox</p>
 <p><b>Employees:</b> 1000+</p>
-<p>How can we help you: New site launching soon using Next.js and we are thinking about using Vercel
+<p><b>How can we help you:</b> New site launching soon using Next.js and we are thinking about using Vercel
 to host. I would like more information about the Enterprise option.</p>
  </li>
-<li><p>There are a few things to consider when choosing a hosting provider for your next project. Vercel has a lot of great features, but it's important to make sure that the provider has the resources and capabilities to support your project. </p>
+<li><p>Hi Joe,</p>
+<p>My name is Dawn, and I'm a product advocate at Vercel. I wanted to reach out because I think our platform could be a great fit for your needs. </p>
+<p>I understand that you're a Sales Lead for barnesandnoble.com, and you're looking to launch a new site soon using Next.js. Vercel can provide the performance and scalability you need for your project.</p>
+<p>Our Enterprise plan is designed specifically for businesses like yours, with features like hourly deployments and priority support. I'd be happy to schedule a call with you to provide more information about this option and how we can help you meet your goals.<p>
+<p>Thanks for your time, </p>
+<p> Dawn </p>
+</li>   
+</ul>
 
-<p>Next.js is a great choice for a new site - it's easy to use and provides a lot of flexibility. Vercel is also a great option for hosting Next.js projects, as we have the resources and experience to support them.</p>
+      <ul className={styles.card_vert}>
+               <li>
+<p><b> <u>Sales Inquiry: Would Pro or Enterprise fit better for my team?</u> </b></p>
+<p><b>Subject:</b> Sales Lead for hipcamp.com </p>
+<p><b>Name:</b> Melissa Wright</p>
+<p><b>Employees:</b> 10-100</p>
+<p><b>How can we help you: </b> We are looking to re-platform our site and are considering Vercel. Would like
+Product Advocate Position at Vercel: Skills Test Template 3
+to evaluate options to see if Pro or Enterprise would fit out needs better. Timeline is Q3. Can you
+help?</p>
+ </li>
+<li><p>Hi Melissa,</p>
+<p>My name is Dawn, and I'm a product advocate at Vercel. I wanted to reach out because I think our platform could be a great fit for your needs. </p>
+<p>I understand that you're a Sales Lead for barnesandnoble.com, and you're looking to launch a new site soon using Next.js. Vercel can provide the performance and scalability you need for your project.</p>
+<p>Our Enterprise plan is designed specifically for businesses like yours, with features like hourly deployments and priority support. I'd be happy to schedule a call with you to provide more information about this option and how we can help you meet your goals.<p>
+<p>Thanks for your time, </p>
+<p> Dawn </p>
 </li>   
 </ul>
 
