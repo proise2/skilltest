95f168da4719b0b90f4eb9a44762755951ac8466
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 14:07:15 -0600

    Update redirect.js

--- pages/redirect.js
+++ pages/redirect.js
@@ -16,6 +16,8 @@ const redirect = () => {
       <Htmlheader title='Redirect' />
       <h2>Push the button below to redirect from the route /redirect</h2>
       <button onClick={onClick}>Click me</button>
+
+<p> I had a little trouble with this one however this is my best attempt.</p>
       <div className={styles.link}><Link href='/email'>Lets move email recommendations</Link></div>
     </div>
   )
