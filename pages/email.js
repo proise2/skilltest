f40555dcd8346a265e31f341174e1d7a5592b8fd
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 11:03:33 -0600

    Update email.js

--- pages/email.js
+++ pages/email.js
@@ -12,20 +12,21 @@ const email = () => {
           <p>Please include one email response for each inquiry, just as if you were responding to a customer.</p>
         </li>
         <li>
-<P><b>Please include one email response for each inquiry, just as if you were responding to a customer.</b></p>
-<P><b>Sales Inquiry:</b> Do I need Enterprise? </p>
-<P><b>Subject:</b> Sales Lead for barnesandnoble.com</p>
-<P><b>Name:</b> Joe Fox</p>
-<P><b>Employees:</b> 1000+</p>
-<P>How can we help you: New site launching soon using Next.js and we are thinking about using Vercel
+<p><b>Please include one email response for each inquiry, just as if you were responding to a customer.</b></p>
+<p><b>Sales Inquiry:</b> Do I need Enterprise? </p>
+<p><b>Subject:</b> Sales Lead for barnesandnoble.com</p>
+<p><b>Name:</b> Joe Fox</p>
+<p><b>Employees:</b> 1000+</p>
+<p>How can we help you: New site launching soon using Next.js and we are thinking about using Vercel
 to host. I would like more information about the Enterprise option.</p>
  </li>
-<li><P>There are a few things to consider when choosing a hosting provider for your next project. Vercel has a lot of great features, but it's important to make sure that the provider has the resources and capabilities to support your project. </P>
+<li><p>There are a few things to consider when choosing a hosting provider for your next project. Vercel has a lot of great features, but it's important to make sure that the provider has the resources and capabilities to support your project. </p>
 
-<P>Next.js is a great choice for a new site - it's easy to use and provides a lot of flexibility. Vercel is also a great option for hosting Next.js projects, as we have the resources and experience to support them.</P>
-      </ul>
-<p>
-      <div className={styles.link}><Link href='/recommendations'>Lets move on to the next question</Link></div>
+<>Next.js is a great choice for a new site - it's easy to use and provides a lot of flexibility. Vercel is also a great option for hosting Next.js projects, as we have the resources and experience to support them.</p>
+</li>   
+</ul>
+
+      <div className={styles.link}><Link href='/recommendations'>Lets move on to the next question</Link>
     </div>
   )
 }
