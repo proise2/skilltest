9ec2b09899fe9d0cdc7e500d2eb448695ed135f7
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 10:23:27 -0600

    Update past.js

--- pages/past.js
+++ pages/past.js
@@ -6,10 +6,11 @@ const past = () => {
   return (
     <div className={styles.container}>
       <Htmlheader title='Past' />
-      <h2>What do you want to learn, or do more of, at work?</h2>
-      <p>Currently I do not have much time outside of tickets and working with our team. I really want to help our product team develop and create better tools for our support team to utilize.</p>
-      <p>There are a few logging utilities that I think could be made better, or else have a nicer UI to display the logs. A lot of the time our logs are out of chronological order, so just creating a tool to put them in an easier to read and in order by their time stamp would help increase troubleshooting speed.</p>
-      <p>As to what I would like to learn, I would love to learn Python, currently this is a language I am not well versed in and would like to build applications that can analyze metrics and data to look for patterns.</p>
+      <h2> Reflection on past experience:</h2>
+      <p>a. In your next role, what are you looking to <b>learn<b/> or do <b>more of?</b></p>
+      <pb. Which (non-Vercel) technical or sales article, guide or lightning talk left the biggest positive
+<b>impression</b> on you?<b>Why?</b> (and please provide the link)
+</p>
       <div className={styles.link}><Link href='/redirect'>Lets try using Next.js</Link></div>
     </div>
   )
