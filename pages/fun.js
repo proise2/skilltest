951422a252eed0b08b79a029b321068b271ffeaa
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 11:46:49 -0600

    Update fun.js

--- pages/fun.js
+++ pages/fun.js
@@ -2,34 +2,46 @@ import Htmlheader from '../components/Htmlheader'
 import Link from 'next/link'
 import styles from '../styles/Question.module.css'
 
-const fun = () => {
+const product = () => {
   return (
     <div className={styles.container}>
-      <Htmlheader title='Fun' />
-      <h2>A user writes in with the following problem: “On our team, GitHub commits by user janedoe@helloworld.com don&apos;t trigger any deployments. Commits by other users work fine. Other projects work fine. Help! What can we do to fix this?” How would you approach debugging this problem? What questions would you ask, either internally or of the user? Write a response which you would send back to this user.</h2>
+      <Htmlheader title='Product Recommendations' />
+      <h2>Things product advocates would do.</h2>
+<p><h3>Top 3 Tasks</h3><p>
       <ul className={styles.card_vert}>
         <li>
-          <p>Looking through Vercel&apos;s documentation I see that there is a KB article regarding <a className={styles.inbedded_link} href="https://vercel.com/support/articles/why-aren-t-commits-triggering-deployments-on-vercel" target='_blank' rel="noreferrer">issues with committing</a>.</p>
+       <p>  <h4>Write direct and concise emails that dig into a prospect's pain points and needs</h4> </p>
+          <p>Identifying and understanding customer needs and desires in order to create product feedback that is valuable to the company. Email is a powerful tool for sales professionals, but it's important to use it effectively. Direct and concise emails that dig into a prospect's pain points and needs are more likely to get a response than long, rambling messages. By focusing on the needs of the prospect, you can create a connection and build trust, which can lead to a sale. </p>
         </li>
         <li>
-          <p>One of the points on this list looks to fit the issue being described by the customer, regarding private repos. The issue may be that the user is not part of the team that is assigned to this specific Vercel project.</p>
+           <p> <h4>Hop on a Zoom call to qualify a prospect or educate them about Vercel</h4></p> 
+          <p>There are a few reasons it's important to hop on a Zoom call to qualify a prospect or educate them about Vercel. First, it's a great way to get to know the prospect and see if they're a good fit for Vercel. Second, it's a great way to educate prospects on the features and benefits of Vercel so they can make an informed decision about whether or not to sign up.</p>
         </li>
         <li>
-          <p>I would start by asking if the user, janedoe@helloworld.com is seeing any error messages when trying to push her commits to the repository. Or if they are seeing any logging in the Vercel dashboard when this user pushes a commit to the repo. (This may come in handy later if we need to further dig into the issue if we cannot easily troubleshoot this issue from the KB)</p>
+           <p>  <h4>Attend weekly enablement meetings to level up your product and sales knowledge</h4></p> 
+<p>Enablement meetings are important because they provide an opportunity for you to learn about new products and sales techniques. By attending these meetings, you can stay up-to-date on the latest changes and developments in your industry. This will help you to better serve your customers and sell your products.</p> 
+        </li>
+  </ul>
+ <p><h3>Bottom 3 Tasks</h3></p>
+     <ul className={styles.card_vert}>
+        <li>
+         <p> <h4>Create Salesforce reports to keep track of data pertinent to the team or your stats</h4></p>
+          <p>Salesforce reporting can seem like a mundane task, but it's important to keep track of your data to ensure your team is meeting its goals. By creating reports that are specific to your needs, you can quickly and easily see how your team is performing.</p>
         </li>
         <li>
-          <p>I would then ask if the github repository is public or private. If it is private, I would ask if this user is part of the Vercel team that belongs to the project they are working on.</p>
+         <p>  <h4>Evaluate an application to help determine if it's commercial or not</h4></p>  
+<p>There are a few reasons why evaluating an application to help determine if it's commercial or not is one of the more least interesting tasks. First, many applications can be used for both commercial and non-commercial purposes. The purpose of an application doesn't always reflect how it will be used.</p>
         </li>
         <li>
-          <p>If the repo is public, or if the user is part of the team on that project, we can then ask if the user made any changes to the vercel.json file or if they made any changes to environmental variables. If so the changes may need to be authorized before the commit will deploy. To check for this, they should look at the pull request where a comment with a link to authorize the deployment will be.</p>
-        </li>
-        <li>
-          <p>Past this point, I am unsure if we would have logs to give us a clearer picture on why the user is unable to deploy their commits. If there are I would check these to see if we are getting an error message indicating why the user is unable to deploy. I would also check previous tickets for this issue and our internal KB to see if we can determine why the users commits are not deploying on this project. We can also ask the team to see if anyone has seen this previously. We can use google to see if there are any stackoverflow or other types of forums where this issue may have been discussed previously. If we run out of runway in our resources for identifying the issue we should escalate the issue to engineering to see if we can determine the root cause of the issue and determine a fix.</p>
+          <p><h4>Research the headquarters & ownership of a company to determine who to route it to</h4></p>
+            <p>There are a few reasons why researching the headquarters ownership of a company to determine who to route it to may not be of interest. </p>
+<p>First, the ownership structure of a company may not be indicative of who is actually responsible for making decisions or handling customer inquiries. </p>
+<p>Second, even if the ownership structure does indicate who is responsible for customer inquiries, the company may have a process in place for routing customer inquiries to the correct department regardless of who the owner is.</p>
         </li>
       </ul>
-      <div className={styles.link}><Link href='/home'>Back to start</Link></div>
+      <div className={styles.link}><Link href='/home'>Back to home</Link></div>
     </div>
   )
 }
 
-export default fun
+export default product
