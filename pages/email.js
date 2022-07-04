4c572e75206e45494201b9673d977abb11c163ab
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 13:58:35 -0600

    Update email.js

--- pages/email.js
+++ pages/email.js
@@ -18,9 +18,10 @@ const email = () => {
 <li><p>Hi Joe,</p>
 <p>My name is Dawn, and I'm a product advocate at Vercel. I wanted to reach out because I think our platform could be a great fit for your needs. </p>
 <p>I understand that you're a Sales Lead for barnesandnoble.com, and you're looking to launch a new site soon using Next.js. Vercel can provide the performance and scalability you need for your project.</p>
-<p>Our Enterprise plan is designed specifically for businesses like yours, with features like hourly deployments and priority support. I'd be happy to schedule a call with you to provide more information about this option and how we can help you meet your goals.</p>
+<p>Our Enterprise plan is designed specifically for businesses like yours, with features like deploying instantly, scaling automatically, and serving personalized content around the globe. </p>
+<p>I'd be happy to schedule a call with you to provide more information about this option and how we can help you meet your goals.</p>
 <p>Thanks for your time, </p>
-<p> Dawn </p>
+<p>Dawn </p>
 </li>   
 
 <li> 
@@ -32,11 +33,12 @@ const email = () => {
  </li>
 
 <li><p>Hi Melissa,</p>
-<p>My name is Dawn, and I'm a product advocate at Vercel. I wanted to reach out because I think our platform could be a great fit for your needs. </p>
-<p>I understand that you're a Sales Lead for barnesandnoble.com, and you're looking to launch a new site soon using Next.js. Vercel can provide the performance and scalability you need for your project.</p>
-<p>Our Enterprise plan is designed specifically for businesses like yours, with features like hourly deployments and priority support. I'd be happy to schedule a call with you to provide more information about this option and how we can help you meet your goals.</p>
-<p>Thanks for your time, </p>
-<p> Dawn </p>
+<p>I hope this email finds you well. My name is Dawn and I'm a product advocate at Vercel.</p>
+<p>Our Pro plan is for small teams with moderate bandwidth and collaboration requirements. Enterprise is for teams seeking greater performance, collaboration, and security. </p>
+<p>I believe Vercel would be a great fit for your company as you look to re-platform your site. I would love to schedule a meeting with you to discuss your needs in more detail and see if our Pro or Enterprise plan would be a better fit for you.</p>
+<p>If you're interested in learning more about Vercel, please let me know and I'll be happy to set up a meeting. Thank you for your time and I look forward to hearing from you soon. </p>
+<p>Best, </p>
+<p>Dawn </p>
 </li>   
 </ul>
 
