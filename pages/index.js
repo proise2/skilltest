550782eae6f2793d86b48f39f8260fdda40614ea
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 12:10:15 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,9 @@ export default function Home() {
   return (
     <div className={styles.home}>
       <Htmlheader title='Skills Test' />
-      <h1>Hello My Name Is Dawn Jama</h1>
-      <p>For your consideration, please follow me through the following questions and scenerios.</p>
+      <h1>Hi, My name Is Dawn Jama</h1>
+      <p>I'm a former teacher and aspiring product advocate.</p>
+      <p>Here you will find my attempt at the product advoate skilltest for Vercel</p>
       <Link href='/product1'>Lets Get Started</Link>
     </div>
   )
