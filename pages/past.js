f629aca0aef2dfa74758336bbbf7745f56335e26
proise2 <108495061+proise2@users.noreply.github.com>
2022-07-04 10:59:35 -0600

    Update past.js

--- pages/past.js
+++ pages/past.js
@@ -8,8 +8,8 @@ const past = () => {
       <Htmlheader title='Past' />
       <h2> Reflection on past experience:</h2>
       <p>a. In your next role, what are you looking to <b>learn<b/> or do <b>more of?</b></p>
-      <pb. Which (non-Vercel) technical or sales article, guide or lightning talk left the biggest positive
-<b>impression</b> on you?<b>Why?</b> (and please provide the link)
+      <p> Which (non-Vercel) technical or sales article, guide or lightning talk left the biggest positive
+<b>impression</b> on you?<b>Why?</b> (and please provide the link)</p>
 </p>
       <div className={styles.link}><Link href='/redirect'>Lets try using Next.js</Link></div>
     </div>
